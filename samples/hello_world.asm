f93c1f3053a7ae39ede1af574ac927c88fff695a
cowboy8625 <cowboy8625@yahoo.com>
2023-11-26 12:24:52 -0600

    feat(assembler): lexer for assembler

---
+++ samples/hello_world.asm
@@ -0,0 +1,16 @@
+entry main
+main:
+  loadimm  %0  1
+  loadimm  %1  1
+  add      %0  %1  %0
+  loadimm  %1  48        ;; Offset of 48 to get to a char
+  add      %0  %1  %0
+  storeu32 %1  %0
+
+  loadimm  %0  1  ;; set register 0 to t for syscall stdout write
+  loadimm  %1  0  ;; set register 1 to location in memeory
+  loadimm  %2  1  ;; set register 1 to location in memeory
+  syscall
+  loadimm  %0  0  ;; set register 0 to 0 for syscall exit
+  loadimm  %1  0  ;; set register 1 to 0 for exit code
+  syscall
---
+++ src/assembler/lexer.zig
@@ -0,0 +1,183 @@
+const std = @import("std");
+
+const TokenType = enum {
+    Number,
+    Keyword,
+    Identifier,
+    PercentSign,
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Colon,
+    Semicolon,
+    String,
+};
+
+const VecToken = std.ArrayList(Token);
+
+const Token = struct {
+    kind: TokenType,
+    lexme: []const u8,
+
+    fn init(kind: TokenType, lexme: []const u8) Token {
+        return Token{
+            .kind = kind,
+            .lexme = lexme,
+        };
+    }
+
+    pub fn format(
+        self: Token,
+        comptime fmt: []const u8,
+        options: std.fmt.FormatOptions,
+        out_stream: anytype,
+    ) !void {
+        if (fmt.len != 0) std.fmt.invalidFmtError(fmt, self);
+        _ = options;
+        try std.fmt.format(out_stream, "Token(kind: {s}, lexme: {s})", .{
+            @tagName(self.kind),
+            self.lexme,
+        });
+    }
+};
+
+// TODO: Clean this function up a bit
+fn checkForKeywords(lexme: []const u8) TokenType {
+    if (std.mem.eql(u8, lexme, "load")) {
+        return .Keyword;
+    } else if (std.mem.eql(u8, lexme, "loadimm")) {
+        return .Keyword;
+    } else if (std.mem.eql(u8, lexme, "storeu8")) {
+        return .Keyword;
+    } else if (std.mem.eql(u8, lexme, "storeu16")) {
+        return .Keyword;
+    } else if (std.mem.eql(u8, lexme, "storeu32")) {
+        return .Keyword;
+    } else if (std.mem.eql(u8, lexme, "inc")) {
+        return .Keyword;
+    } else if (std.mem.eql(u8, lexme, "push")) {
+        return .Keyword;
+    } else if (std.mem.eql(u8, lexme, "pop")) {
+        return .Keyword;
+    } else if (std.mem.eql(u8, lexme, "add")) {
+        return .Keyword;
+    } else if (std.mem.eql(u8, lexme, "syscall")) {
+        return .Keyword;
+    }
+    return .Identifier;
+}
+
+fn lineComment(ip: *usize, buf: []const u8) void {
+    while (ip.* < buf.len and buf[ip.*] != '\n') {
+        ip.* += 1;
+    }
+}
+
+fn isLineComment(ip: usize, buf: []const u8) bool {
+    const next = peek(ip, buf) orelse return false;
+    return buf[ip] == '/' and next == '/';
+}
+
+fn peek(ip: usize, buf: []const u8) ?u8 {
+    if (ip < buf.len) {
+        return buf[ip];
+    } else {
+        return null;
+    }
+}
+
+pub fn lexer(src: []const u8) !VecToken {
+    var tokens: VecToken = VecToken.init(std.heap.page_allocator);
+    var ip: usize = 0;
+    while (ip < src.len) {
+        if (std.ascii.isDigit(src[ip])) {
+            const result = try number(&ip, src);
+            const token = Token.init(.Number, result);
+            try tokens.append(token);
+            ip += 1;
+        } else if (std.ascii.isAlphabetic(src[ip]) or src[ip] == '_') {
+            const result = try identifier(&ip, src);
+            const kind = checkForKeywords(result);
+            const token = Token.init(kind, result);
+            try tokens.append(token);
+            ip += 1;
+        } else if (isLineComment(ip, src)) {
+            lineComment(&ip, src);
+            ip += 1;
+        } else if (src[ip] == '"') {
+            const result = try string(&ip, src);
+            const token = Token.init(.String, result);
+            try tokens.append(token);
+            ip += 1;
+        } else if (src[ip] == '%') {
+            const token = Token.init(.PercentSign, "%");
+            try tokens.append(token);
+            ip += 1;
+        } else if (src[ip] == '+') {
+            const token = Token.init(.Plus, "+");
+            try tokens.append(token);
+            ip += 1;
+        } else if (src[ip] == '-') {
+            const token = Token.init(.Minus, "-");
+            try tokens.append(token);
+            ip += 1;
+        } else if (src[ip] == '*') {
+            const token = Token.init(.Star, "-");
+            try tokens.append(token);
+            ip += 1;
+        } else if (src[ip] == '/') {
+            const token = Token.init(.Slash, "/");
+            try tokens.append(token);
+            ip += 1;
+        } else if (src[ip] == ':') {
+            const token = Token.init(.Colon, ":");
+            try tokens.append(token);
+            ip += 1;
+        } else if (src[ip] == ';') {
+            const token = Token.init(.Semicolon, ";");
+            try tokens.append(token);
+            ip += 1;
+        } else if (std.ascii.isWhitespace(src[ip])) {
+            ip += 1;
+        } else {
+            std.debug.panic("error: unknonw char '{c}' {d} unimplemented in assember tokenizer", .{ src[ip], ip });
+            break;
+        }
+    }
+    return tokens;
+}
+
+fn number(ip: *usize, buf: []const u8) ![]const u8 {
+    const String = std.ArrayList(u8);
+    var lexme: String = String.init(std.heap.page_allocator);
+    while (std.ascii.isDigit(buf[ip.*])) {
+        try lexme.append(buf[ip.*]);
+        ip.* += 1;
+    }
+    ip.* -= 1;
+    return lexme.items;
+}
+
+fn identifier(ip: *usize, buf: []const u8) ![]const u8 {
+    const String = std.ArrayList(u8);
+    var lexme: String = String.init(std.heap.page_allocator);
+    while (std.ascii.isAlphanumeric(buf[ip.*])) {
+        try lexme.append(buf[ip.*]);
+        ip.* += 1;
+    }
+    ip.* -= 1;
+    return lexme.items;
+}
+
+fn string(ip: *usize, buf: []const u8) ![]const u8 {
+    const String = std.ArrayList(u8);
+    var lexme: String = String.init(std.heap.page_allocator);
+    ip.* += 1;
+    while (buf[ip.*] != '"') {
+        try lexme.append(buf[ip.*]);
+        ip.* += 1;
+    }
+    ip.* += 1;
+    return lexme.items;
+}
